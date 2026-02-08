2bbb6e0cd409f2ee02b0578ee2e528eefd951957
Phi <ptp.thamboonrak@gmail.com>
2026-02-08 23:51:57 +0700

    refactor: clean up formatting and comments in db.mjs

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,9 +1,11 @@
-// Create PostgreSQL Connection Pool here !
+// db.mjs
 import { Pool } from "pg";
 
 const connectionPool = new Pool({
-  connectionString:
-    process.env.CONNECTION_STRING,
+  connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false, // Supabase ต้องการ SSL
+  },
 });
 
 export default connectionPool;
