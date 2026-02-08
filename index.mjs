b35404f0ce2db7a580b087af8597d94dfd7ac386
Phi <ptp.thamboonrak@gmail.com>
2026-02-08 22:51:42 +0700

    feat: rewrite server setup in index.mjs with Express and add API endpoints

--- index.mjs
+++ index.mjs
@@ -1,2 +1,94 @@
-import app from "./api/index.mjs";
-export default app;
+import express from "express";
+import "dotenv/config";
+import connectionPool from "../utils/db.mjs";
+import cors from "cors";
+
+
+const app = express();
+const port = process.env.PORT || 4002;
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Frontend local (Vite)
+      "http://localhost:3000", // Frontend local (React แบบอื่น)
+      "https://personal-blog-react-32p2.app", // Frontend ที่ Deploy แล้ว
+      "https://personal-blog-react-32p2-git-dev-phis-projects-e10d8e3b.vercel.app",
+    ],
+    methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
+  })
+);
+
+app.get("/test", (req,res)=>{
+    return res.json({message:"Server API IS working"});
+});
+
+app.get("/profiles", (req, res) => {
+  return res.status(200).json({
+    data: {
+      name: "john",
+      age: 20
+    }
+  });
+});
+
+app.post("/posts", async (req, res) => {
+  try{
+     const { title,image,category_id,description, content, status_id } = req.body;
+    if (!title || !image || !category_id || !description || !content || !status_id) {
+      return res.status(400).json({
+        message: "Server could not create assignment because there are missing data from client" 
+      });
+    }
+   const newPost = {
+     ...req.body,
+   };
+   await connectionPool.query(
+     `INSERT INTO posts ( title,image,category_id,description, content, status_id)
+	   VALUES ($1, $2, $3, $4, $5, $6)`,
+     [
+       newPost.title,
+       newPost.image,
+       newPost.category_id,
+       newPost.description,
+       newPost.content,
+       newPost.status_id,
+     ]
+   );
+   return res.status(201).json({
+     message: "Created post successfully",
+   });
+  }
+  catch(error){
+    console.log(error)
+    return res.status(500).json({ message: "Server could not create assignment because database connection" });
+  }
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
+app.get("/posts", async (req,res)=>{
+  try{
+    const results = await connectionPool.query(`SELECT * FROM posts`);
+    res.status(200).json({
+      data: results.rows
+    })
+  }
+  catch(error){
+    console.log(error)
+    res.status(500).json({
+      message: "Server could not fetch posts"
+    })
+  }
+})
+
+
+
+// app.listen(port, () => {
+//   console.log(`🚀 Server is running at ${port}`);
+// });
+
+export default app
